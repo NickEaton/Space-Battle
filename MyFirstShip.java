5ec17ced7e6f308f085e106b788132bb4008a65d
NickEaton <nickeaton99@gmail.com>
2016-05-13 08:30:42 -0700

    Add files via upload

--- MyFirstShip.java
+++ MyFirstShip.java
@@ -7,7 +7,10 @@ import ihs.apcs.spacebattle.*;
 import ihs.apcs.spacebattle.Point;
 import ihs.apcs.spacebattle.commands.*;
 
+//Code for a client spacecraft
 public class MyFirstShip extends BasicSpaceship {
+	
+	//private fields for manipulating ship movement
 	private int width;
 	private int height;
 	private Point p = new Point(0, 0);
@@ -21,10 +24,12 @@ public class MyFirstShip extends BasicSpaceship {
 	private boolean Rotate = false;
 	private boolean GoToCenter = true;
 	
+	//Join the server with the IP address as the first string parameter, and another parameter of the new Ship
 	public static void main(String[] args) {
 		TextClient.run("10.136.35.81", new MyFirstShip());
 	}
 	
+	//Register the ship and get basic map data
 	public RegistrationData registerShip(int numImages, int worldWidth, int worldHeight) {
 		this.width = worldWidth;
 		this.height = worldHeight;
@@ -32,12 +37,18 @@ public class MyFirstShip extends BasicSpaceship {
 		return new RegistrationData("The Meme Machine", new Color(255, 0, 0), 9);
 	}
 	
+	//Private method used to get to the middle of the map
 	private double getThrustDuration(double distance) {
 		return distance/60;
 	}
+	
+	//Returns a new command for the ship to accept
 	public ShipCommand getNextCommand(BasicEnvironment env) {
+		
+		//Get the current status (location, velocity, etc) of the ship
 		ObjectStatus ship = env.getShipStatus();
 		
+		//Moves the ship in a straight line to the right
 		if(MoveBehaviorLine) { //Straight Line Movement
 			double rad = env.getShipStatus().getMovementDirection();
 			if(rad<0) {return new RotateCommand(Math.abs(rad));}
@@ -45,6 +56,7 @@ public class MyFirstShip extends BasicSpaceship {
 			return new ThrustCommand('F', 100, 1.0);
 		}
 		
+		//Goes to a specific point in space, indicated by the private field 'p'
 		if(GoToPoint) {
 			if(NStop) {NStop = false; return new IdleCommand(1);}
 			
@@ -56,16 +68,19 @@ public class MyFirstShip extends BasicSpaceship {
 			return new RotateCommand(ship.getPosition().getAngleTo(p) - ship.getOrientation());	
 		}
 		
+		//Returns an idle command of time val
 		if(Pause) {
 			Pause = false;
 			return new IdleCommand(val);
 		}
 		
+		//Breaks the ship to a stop
 		if(Break) {
 			Break = false;
 			return new BrakeCommand(0.0);
 		}
 		
+		//Rotates the ship to each corner using a switch-case statement
 		if(Rotate) {
 			switch(c) {
 				case 1:
@@ -83,21 +98,33 @@ public class MyFirstShip extends BasicSpaceship {
 			}
 		}
 		
+		//Moves the ship to the center of the map, and attempts to keep it there
 		if(GoToCenter) {
+			
+			//Prints data for debugging
 			System.out.println(ship.getPosition().getAngleTo(new Point(width/2, height/2))+" "+ship.getOrientation());
+			
+			//Get the angle to the center of the map
 			int pos = ship.getPosition().getAngleTo(new Point(width/2, height/2));
+			
+			//Fix the coordinates as they use different systems
 			if(pos <0) pos = 360+pos;
+			
+			//Thrusts the ship forward and then idles if the angles match
 			if(pos - Math.round(ship.getOrientation()) <= 1 && pos - Math.round(ship.getOrientation()) >= -1) {
 				Break = true;
 				Pause = true;
 				val = 8;
 				return new ThrustCommand('B', getThrustDuration(ship.getPosition().getDistanceTo(new Point(width/2, height/2))), 1.0);
 			}
+			
+			//Else rotates the ship so it's facing the center
 			Pause = true;
 			val = .5;
 			return new RotateCommand(ship.getPosition().getAngleTo(new Point(width/2, height/2)) - ship.getOrientation());
 		}
 		
+		//Returns an idle command if none of the other behaviors are active
 		return new IdleCommand(.5);
 	}
 }
